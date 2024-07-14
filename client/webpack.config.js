ee5734959a27d9771eb4d05e3552128e1cf0489d
zz <prizebots@pm.me>
2024-07-14 17:55:11 -0400

    ok

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './client/index.html', // Path to your index.html file
+      template: 'index.html', // Path to your index.html file
     }),
   ],
 };
