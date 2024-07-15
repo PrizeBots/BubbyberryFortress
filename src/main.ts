767a80ab44acf750b614871bdf469082620a7a45
zz <prizebots@pm.me>
2024-07-15 14:48:25 -0400

    ok

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import { IoAdapter } from '@nestjs/platform-socket.io';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -12,11 +11,15 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
+  
   const port = process.env.PORT || 3000;
+  console.log(`Using port: ${port}`);  // Log the port for verification
+
   app.useWebSocketAdapter(new IoAdapter(app));
 
   await app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
   });
 }
+
 bootstrap();
