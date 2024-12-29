197dac1cffdd88540d12c7e8a9abdb414352a0ae
zz <prizebots@pm.me>
2024-12-28 23:41:17 -0500

    ok

--- client/Game.js
+++ client/Game.js
@@ -111,7 +111,7 @@ class Game extends Phaser.Scene {
         }
 
         const port = process.env.PORT || 3000;
-        this.socket = io(`http://localhost:${port}`);
+       // this.socket = io(`http://localhost:${port}`);
         console.log("CLIENT ON WHAT PORT")
        // this.socket = io('https://bbf-kn8o.onrender.com');
         this.socketManager = new SocketManager(this);
